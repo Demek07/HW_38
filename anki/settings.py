544e05062df09dcf245eaf346c05b7ef1ef9e491
Demek07 <136734449+Demek07@users.noreply.github.com>
2024-05-31 15:22:14 +0300

    upd settings.py

--- anki/settings.py
+++ anki/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG')
 
 
-ALLOWED_HOSTS = ['flip-cards.ru', 'www.flip-cards.ru', 'localhost', '127.0.0.1', '213.171.10.225']
+ALLOWED_HOSTS = ['flip-cards.ru', 'www.flip-cards.ru', 'localhost', '127.0.0.1', '89.23.115.229']
 
 CSRF_TRUSTED_ORIGINS = ['https://flip-cards.ru', 'https://www.flip-cards.ru']
 
